5b7393aea0fbae514067fe25431d95a8fc21b69c
drivera129 <156861448+drivera129@users.noreply.github.com>
2024-06-25 10:55:07 -0400

    Add files via upload

---
+++ problems/01-variables.js
@@ -0,0 +1,48 @@
+/*
+Time to practice variables! Follow the comments for instructions. Be sure to
+test your work by running your code and the test specs!
+*/
+
+// 1. Declare a variable called `firstName` and assign it your first name
+//    as a string:
+
+let firstName = 'Diego';
+
+
+// 2. Declare a variable called `lastName` and assign it your last name
+//    as a string:
+
+let lastName = 'Rivera';
+
+
+// 3. Declare a variable called `age`:
+
+let age;
+
+
+// 4. Print out the `firstName`, `lastName`, and `age` variables. What
+//    do you expect them to be when they get printed?
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+
+// 5. Assign the `age` variable to a number:
+
+age = 34;
+
+
+// 6. Print out the `firstName`, `lastName`, and `age` variables. What
+//    do you expect them to be when they get printed?
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+
+/******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
+const obj = {};
+try { obj.firstName = firstName; } catch {}
+try { obj.lastName = lastName; } catch {}
+try { obj.age = age; } catch {}
+module.exports = obj;
